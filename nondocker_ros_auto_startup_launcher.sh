63b11978a867c866b39b3590da32d22bb055acc3
anht-nguyen <tuananh.nguyen28898@gmail.com>
2025-07-04 23:09:46 -0400

    Remove unnecessary comments and streamline tmux window creation in startup script

--- nondocker_ros_auto_startup_launcher.sh
+++ nondocker_ros_auto_startup_launcher.sh
@@ -3,11 +3,6 @@ set -e
 
 # Auto startup script for ROS on ubuntu 20 machine (non-docker) in a tmux session
 
-# 0) load ROS & workspace overlays
-# source /opt/ros/noetic/setup.bash
-# source /catkin_ws/devel/setup.bashem
-# udevadm control --reload-rules && udevadm trigger # ensure udev inside the container sees the camera
-
 # 1) Start tmux session named “ros”
 tmux new-session -d -s ros #'roscore'
 
@@ -23,14 +18,9 @@ tmux new-window -t ros:3 -n vision 'roslaunch flo_vision arm_hand_tracker_launch
 sleep 2
 tmux new-window -t ros:4 -n vision_monitor 'rostopic echo /arm_hand_tracker/pose_score' #uncomment to monitor the arm hand tracker node
 sleep 5
-tmux new-window -t ros:5 -n hw_if       'roslaunch flo_humanoid dual_arm_hardware.launch'
+tmux new-window -t ros:5 -n hw_if 'roslaunch flo_humanoid dual_arm_hardware.launch'
+sleep 2
+tmux new-window -t ros:6 -n face 'roslaunch flo_face flo_face_launcher.launch'
 sleep 2
 tmux new-window -t ros:6 -n game_runner 'roslaunch flo_core simonsays_launcher_prod.launch'
 
-
-# 3) (optional) Attach so you see the tmux panes on “docker attach”
-# tmux attach -t ros
-
-
-# (optional) leave tmux detached, then block forever
-# exec tail -f /dev/null
